a3d704ac052f447663028517e7b4f38cddf5522f
Raiqa Rasool <86972954+RaiqaRasool@users.noreply.github.com>
2022-03-14 22:58:10 +0500

    Delete Desktop directory

--- Desktop/monsters-rolodex/monsters-rolodex/src/components/card-list-component/card-list.component.jsx
+++
@@ -1,9 +0,0 @@
-import Card from "../card-component/card.component";
-import "./card-list.styles.css";
-
-export default function CardList(props){
-  return (<div className="card-list">
-       {props.monsters.map(monster=><Card monster={monster}/>)}
-  </div>);
-
-}
--- Desktop/monsters-rolodex/monsters-rolodex/src/components/search-field.component/search-field.component.jsx
+++
@@ -1,6 +0,0 @@
-import "./search-field.styles.css";
-export default function SearchField(props)
-{
-    return (<input className="search-field" type="search" placeholder={props.placeholder} onChange={props.handleChange}></input>);
-
-}
